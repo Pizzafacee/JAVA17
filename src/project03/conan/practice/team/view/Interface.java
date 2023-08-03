722ce4b9039a69d8068a0d997e7377d6f0272697
pizza <pizzaface2@live.com>
2023-08-03 23:13:51 +0800

    java17 7/30

---
+++ src/project03/conan/practice/team/view/Interface.java
@@ -0,0 +1,15 @@
+package project03.conan.practice.team.view;
+
+public class Interface {
+    public static void main(String[] args) {
+        enterInterface();
+    }
+
+    private static void enterInterface() {
+        boolean flag = true;
+        do {
+            flag = false;
+
+        } while (flag);
+    }
+}
