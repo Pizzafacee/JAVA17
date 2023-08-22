357914be33337193d35a1a5c2b40c29f76328976
pizza <pizzaface2@live.com>
2023-08-22 23:41:13 +0800

    java17 7/30

---
+++ src/chapter35/ArrayListStructure.java
@@ -0,0 +1,14 @@
+package chapter35;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListStructure {
+    @Test
+    public void test1() {
+        List<String> objects = new ArrayList<>();
+    }
+
+}
