454b0905e2f4c68a85483a2d512cf270359dc73a
pizza <pizzaface@live.com>
2023-09-05 14:12:39 +0800

    6/30

--- src/chaptertest/ArrayTest.java
+++ src/chaptertest/ArrayTest.java
@@ -2,6 +2,11 @@ package chaptertest;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+
 public class ArrayTest {
     @Test
     public void test1() {
@@ -11,6 +16,10 @@ public class ArrayTest {
         students[0] = new Student("zhangsan", 11);
         System.out.println(student);
         System.out.println(students[0]);
+        HashMap<Object, Object> objectObjectHashMap = new HashMap<>();
+        ArrayList<Object> objects = new ArrayList<>();
+        Iterator<Object> iterator = objects.iterator();
+        Object next = iterator.next();
 
     }
 }
